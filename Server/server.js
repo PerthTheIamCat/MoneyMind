85dddaf94be2c73237f9094102563809d4f8e6ac
Maruki119 <93820527+Maruki119@users.noreply.github.com>
2025-01-27 18:41:55 +0700

    Server: Add Send Email API and Connect Backend to database, add .env.example file

--- Server/server.js
+++ Server/server.js
@@ -1,10 +1,48 @@
-const express = require('express')
-const app = express()
+const express = require('express')  // Import express
+const mysql = require('mysql')      // Import mysql
+const app = express()               // Create express app
+const bcrypt = require('bcrypt')    // Import bcrypt for password hashing
+require('dotenv').config();        // Import dotenv for environment variables
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000')
+const port = process.env.PORT || 3000
+
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS, //Enter MySQL password in .env file
+    database: process.env.DB_NAME
 })
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
     res.send('Hello World')
+})
+
+app.get('/users', (req, res) => {
+    db.query('SELECT * FROM users', (err, result) => {
+        if (err) {
+            return res.status(500).json({ message: 'Database query failed', error: err.message });
+        }
+        res.status(200).json(result);
+    });
+});
+
+app.get('/users/:id', (req, res) => {
+    db.query(
+        'SELECT * FROM users WHERE UserID = ?',
+        [req.params.id],
+        function(err, result) {
+            res.json(result)
+        }
+    )
+})
+
+const sendEmailRouter = require('./routes/sendEmail')
+
+app.use('/sendEmail', sendEmailRouter)
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
 })
