29d3911a1167af5975571bdc5c9a819d9be4e614
PerthTheIamCat <thongcum2546@gmail.com>
2025-01-27 13:36:36 +0700

    add logo and font

--- Client/app/index.tsx
+++ Client/app/index.tsx
@@ -6,9 +6,25 @@ import { Link } from "expo-router";
 export default function Index() {
   return (
     <ThemedView className="!justify-start">
-      <Image source={require('@/assets/logos/LOGO.png')} style={{ width: '80%', height:400, marginTop: 100 }} contentFit="contain"/>
-      <ThemedText>WELCOME SCREEN</ThemedText>
-      <Link href={"/SignUp"}>Sign Up</Link>
+      <Image
+        source={require("@/assets/logos/LOGO.png")}
+        style={{
+          width: "80%",
+          height: "50%",
+          maxHeight: 400,
+          marginTop: "15%",
+        }}
+        contentFit="contain"
+      />
+      <ThemedView className="!justify-start !items-start w-full ml-[20%]">
+        <ThemedText className="text-[#55A630] text-[48px]">
+          MoneyMind
+        </ThemedText>
+        <ThemedText className="text-[#AACC00] text-[32px] text-wrap max-w-[250px]">
+          Unlock Your Financial Dreams Empowering Your Journey to Wealth and
+          Freedom
+        </ThemedText>
+      </ThemedView>
     </ThemedView>
   );
 }
