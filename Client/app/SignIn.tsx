709c5499ce1c54139916ba8393e3474df8885179
PerthTheIamCat <thongcum2546@gmail.com>
2025-01-27 13:02:09 +0700

    add logo to welcome screen

---
+++ Client/app/SignIn.tsx
@@ -0,0 +1,11 @@
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+import { Link } from "expo-router";
+export default function Index() {
+  return (
+    <ThemedView >
+      <ThemedText>SignUp SCREEN</ThemedText>
+      <Link href={"/(tabs)"}>Sign In</Link>
+    </ThemedView>
+  );
+}
--- Client/app/index.tsx
+++ Client/app/index.tsx
@@ -1,10 +1,12 @@
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
+import { Image } from "expo-image";
 import { Link } from "expo-router";
 
 export default function Index() {
   return (
-    <ThemedView >
+    <ThemedView className="!justify-start">
+      <Image source={require('@/assets/logos/LOGO.png')} style={{ width: '80%', height:400, marginTop: 100 }} contentFit="contain"/>
       <ThemedText>WELCOME SCREEN</ThemedText>
       <Link href={"/SignUp"}>Sign Up</Link>
     </ThemedView>
