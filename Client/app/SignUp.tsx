0828dd9aff32ef3785ecadbfe1fec5beb8d0ca54
PerthTheIamCat <thongcum2546@gmail.com>
2025-01-30 22:28:59 +0700

    refactor SignUp screen layout and enhance input handling; implement keyboard avoidance and error display

--- Client/app/SignUp.tsx
+++ Client/app/SignUp.tsx
@@ -2,60 +2,127 @@ import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
 import { ThemedButton } from "@/components/ThemedButton";
 import { Image } from "expo-image";
-import { router } from "expo-router";
-import { ThemedInput } from "@/components/ThemedInput";
+import {
+  KeyboardAvoidingView,
+  ScrollView,
+  TextInput,
+  SafeAreaView,
+  Platform,
+} from "react-native";
+import { useColorScheme } from "react-native";
+import { useState } from "react";
+import Entypo from "@expo/vector-icons/Entypo";
+
 export default function Index() {
+  const theme = useColorScheme();
+
+  const [username, setUsername] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [passwordConfirmation, setPasswordConfirmation] = useState<string>("");
+  const [errorUsername, setErrorUsername] = useState<string>("awdawdadw");
+  const [errorEmail, setErrorEmail] = useState<string>("awdawd");
+  const [errorPassword, setErrorPassword] = useState<string>("awdawd");
+  const [errorPasswordConfirmation, setErrorPasswordConfirmation] =
+    useState<string>("awdawd");
+
   return (
-    <ThemedView className="!justify-start">
-      <Image
-        source={require("@/assets/logos/LOGO.png")}
-        style={{
-          width: "100%",
-          height: "20%",
-          maxHeight: 150,
-          marginTop: "25%",
-        }}
-        contentFit="contain"
-      />
-      <ThemedView className="!justify-start !items-start mt-[7%] w-full pl-24 " >
-        <ThemedText className="text-[28px] sm:text-[34px] max-w-[80%] font-bold mb-8 ">
-          Sign Up
-        </ThemedText>
-        <ThemedInput
-          title="Username"
-          autoComplete="username"
-          keyboardType="default"
-        />
-        <ThemedInput
-          title="Email"
-          autoComplete="email"
-          keyboardType="email-address"
-        />
-        <ThemedInput
-          title="Password"
-          autoComplete="password"
-          keyboardType="visible-password"
-        />
-        <ThemedInput
-          title="Confirm Password"
-          autoComplete="password"
-          keyboardType="visible-password"
-        />
-        <ThemedText className="text-[12px] sm:text-[16px] max-w-[80%] font-bold mb-4 ">
-          Sagree our term of service
-        </ThemedText>
-        <ThemedText className="text-[12px] sm:text-[16px] max-w-[80%] font-bold ">
-          receive notification on email
-        </ThemedText>
-      </ThemedView>
-      <ThemedView className="flex-col w-full max-h-[10%] gap-4 mb-10">
-        <ThemedButton mode="normal" onPress={() => router.push("/SignIn")} className="max-w-[250px] w-[50%]">
-          Sign In
-        </ThemedButton>
-        <ThemedButton mode="confirm" onPress={() => router.push("/SignUp")}className="max-w-[250px] w-[50%] ">
-          Sign Up
-        </ThemedButton>
-      </ThemedView>
-    </ThemedView>
+    <SafeAreaView
+      className={`flex-1 ${theme === "dark" ? "bg-[#2F2F2F]" : "bg-[#F2F2F2]"}`}
+    >
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+      >
+        <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+          <ThemedView>
+            <Image
+              source={require("@/assets/logos/LOGO.png")}
+              style={{
+                width: 150,
+                height: 150,
+                marginTop: 70,
+              }}
+            />
+            <ThemedView className="w-96 mt-5 px-5 gap-5">
+              <ThemedText className="text-2xl font-bold w-full text-start">
+                Sign Up
+              </ThemedText>
+
+              <ThemedView className="w-full">
+                <ThemedText className="text-xl font-bold w-full">
+                  Username
+                </ThemedText>
+                <TextInput
+                  autoComplete="username"
+                  className="h-10 bg-[#D9D9D9] text-[#2F2F2F] rounded-xl p-2 w-full"
+                  onChangeText={setUsername}
+                />
+                {errorUsername ? (
+                  <ThemedText className="text-red-500 w-full">
+                    {errorUsername}
+                  </ThemedText>
+                ) : null}
+              </ThemedView>
+
+              <ThemedView className="w-full">
+                <ThemedText className="text-xl font-bold w-full">
+                  Email
+                </ThemedText>
+                <TextInput
+                  autoComplete="email"
+                  className="h-10 bg-[#D9D9D9] text-[#2F2F2F] rounded-xl p-2 w-full"
+                  onChangeText={setEmail}
+                />
+                {errorEmail ? (
+                  <ThemedText className="text-red-500 w-full">
+                    {errorEmail}
+                  </ThemedText>
+                ) : null}
+              </ThemedView>
+
+              <ThemedView className="w-full">
+                <ThemedText className="text-xl font-bold w-full">
+                  Password
+                </ThemedText>
+                <ThemedView className="flex-row items-center bg-[#D9D9D9] rounded-xl p-2">
+                  <TextInput
+                    autoComplete="password"
+                    className="flex-1 text-[#2F2F2F]"
+                    secureTextEntry
+                    onChangeText={setPassword}
+                  />
+                  <Entypo className="px-2" name="eye" size={24} color="black" />
+                </ThemedView>
+                {errorPassword ? (
+                  <ThemedText className="text-red-500 w-full">
+                    {errorPassword}
+                  </ThemedText>
+                ) : null}
+              </ThemedView>
+
+              <ThemedView className="w-full">
+                <ThemedText className="text-xl font-bold w-full">
+                  Confirm Password
+                </ThemedText>
+                <ThemedView className="flex-row items-center bg-[#D9D9D9] rounded-xl p-2">
+                  <TextInput
+                    autoComplete="password"
+                    className="flex-1 text-[#2F2F2F]"
+                    secureTextEntry
+                    onChangeText={setPasswordConfirmation}
+                  />
+                  <Entypo className="px-2" name="eye" size={24} color="black" />
+                </ThemedView>
+                {errorPasswordConfirmation ? (
+                  <ThemedText className="text-red-500 w-full">
+                    {errorPasswordConfirmation}
+                  </ThemedText>
+                ) : null}
+              </ThemedView>
+            </ThemedView>
+          </ThemedView>
+        </ScrollView>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
   );
 }
--- Client/app/index.tsx
+++ Client/app/index.tsx
@@ -3,32 +3,37 @@ import { ThemedView } from "@/components/ThemedView";
 import { ThemedButton } from "@/components/ThemedButton";
 import { Image } from "expo-image";
 import { router } from "expo-router";
+import { ScrollView } from "react-native";
 
 export default function Index() {
   return (
-    <ThemedView className="!justify-start">
+    <ThemedView className="!justify-start h-full max-h-full overflow-y-auto">
       <Image
         source={require("@/assets/logos/LOGO.png")}
         style={{
           width: "100%",
-          height: "30%",
-          maxHeight: 400,
-          marginTop: "25%",
+          height: "50%",
+          maxHeight: 300,
+          marginTop: "20%",
         }}
         contentFit="contain"
       />
-      <ThemedView className="!justify-start !items-start mt-[10%] w-full pl-10">
-        <ThemedText className="!text-[#55A630] text-[40px] sm:text-[48px] max-w-[80%] font-bold">
+      <ThemedView className="!items-start pl-10 w-full font-bold">
+        <ThemedText className="text-5xl my-10 !text-[#55A630]">
           MoneyMind
         </ThemedText>
-        <ThemedText className="!text-[#AACC00] text-[24px] sm:text-[32px] text-wrap max-w-[70%] font-bold">
+        <ThemedText className="max-w-[60%] text-3xl text-wrap font-bold !text-[#AACC00]">
           Unlock Your Financial Dreams Empowering Your Journey to Wealth and
           Freedom
         </ThemedText>
       </ThemedView>
-      <ThemedView className="flex-row w-full max-h-[10%] gap-10 mb-10 !items-start">
-        <ThemedButton mode="normal" onPress={() => router.push('/SignIn')}>Sign In</ThemedButton>
-        <ThemedButton mode="confirm" onPress={() => router.push('/SignUp')}>Sign Up</ThemedButton>
+      <ThemedView className="flex-row gap-5 h-fit absolute bottom-10 w-full">
+        <ThemedButton className="w-[45%] h-14" mode="normal" onPress={() => router.push("/SignIn")}>
+          Sign In
+        </ThemedButton>
+        <ThemedButton className="w-[40%] h-14" mode="confirm" onPress={() => router.push("/SignUp")}>
+          Sign Up
+        </ThemedButton>
       </ThemedView>
     </ThemedView>
   );
