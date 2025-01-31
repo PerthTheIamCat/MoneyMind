62d0551734585fa726a7a98846e3e46e9f31aeec
PerthTheIamCat <thongcum2546@gmail.com>
2025-01-31 15:03:36 +0700

    add ThemedSafeAreaView component for improved layout and keyboard handling

---
+++ Client/components/ThemedSafeAreaView.tsx
@@ -0,0 +1,24 @@
+import { KeyboardAvoidingView, ScrollView, Platform, useColorScheme } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import React, { ReactNode } from 'react';
+
+export type ThemedSafeAreaViewProps = {
+    children?: ReactNode;
+}
+
+export function ThemedSafeAreaView({ children } : ThemedSafeAreaViewProps) {
+    const theme = useColorScheme();
+
+    return (
+        <SafeAreaProvider>
+            <SafeAreaView style={{ flex: 1, backgroundColor: theme === 'dark' ? '#2F2F2F' : '#F2F2F2' }}>
+                <KeyboardAvoidingView style={{ flex: 1, backgroundColor: theme === 'dark' ? '#2F2F2F' : '#F2F2F2' }}
+                    behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+                    <ScrollView style={{ flex: 1, backgroundColor: theme === 'dark' ? '#2F2F2F' : '#F2F2F2' }}>
+                        {children}
+                    </ScrollView>
+                </KeyboardAvoidingView>
+            </SafeAreaView>
+        </SafeAreaProvider>
+    );
+}
