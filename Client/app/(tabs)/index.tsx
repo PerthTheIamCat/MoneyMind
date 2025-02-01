548a3e32b5651332dc1f0e1b80b549c8dbffc3ea
SoHizz21 <sohizz1201@gmail.com>
2025-02-02 03:05:37 +0700

    Change State Go plan retirement

--- Client/app/(tabs)/index.tsx
+++ Client/app/(tabs)/index.tsx
@@ -16,6 +16,9 @@ export default function Index() {
   const componentcolor = theme === "dark" ? "!bg-[#8f8f8f]" : "!bg-[#d8d8d8]";
   const componenticon = theme === "dark" ? "#f2f2f2" : "#2f2f2f";
 
+  
+  const [checkretireData,setCheckRetireData]=useState(false);
+  const [retire,setretire]=useState(5000);
 
   return (
     <ThemedSafeAreaView>
@@ -34,25 +37,42 @@ export default function Index() {
         </ThemedText>
       </ThemedView>
 
-
+      {/* check retire have data */}
+      {!checkretireData ? (
+        <ThemedView className="mt-3">
+          <ThemedButton className={`${componentcolor} h-40 w-4/5 rounded-[20]`} onPress={() => setCheckRetireData(true)}>
+              <ThemedView className="bg-transparent">
+                <ThemedText className="font-bold">
+                  Your Monthly Save Goal
+                </ThemedText>
+                <AntDesign name="filetext1" size={50} color={`${componenticon}`} className="m-3"/>
+                <ThemedText className="mx-5 text-center font-bold">
+                  Let’s get started with your first 
+                  retirement plan!
+                </ThemedText>
+              </ThemedView>
+          </ThemedButton>
+        </ThemedView>
+      ):(
       <ThemedView className="mt-3">
-        <ThemedButton className={`${componentcolor} h-40 w-4/5 rounded-[20]`}>
-            <ThemedView className="bg-transparent">
-              <ThemedText className="font-bold">
-                Your Monthly Save Goal
-              </ThemedText>
-              <AntDesign name="filetext1" size={50} color={`${componenticon}`} className="m-3"/>
-              <ThemedText className="mx-5 text-center font-bold">
-                Let’s get started with your first 
-                retirement plan!
-              </ThemedText>
-            </ThemedView>
-        </ThemedButton>
+        <ThemedView className={`${componentcolor} h-40 w-4/5 rounded-[20]`}>         
+          <ThemedText className="font-bold">
+            Your Monthly Save Goal
+          </ThemedText>
+          <ThemedText className="h-1/2 w-10 align-middle font-bold">{retire}</ThemedText>
+          <ThemedText className="mx-5 text-center font-bold">
+           Goal 9.0k
+          </ThemedText>
+        </ThemedView>
       </ThemedView>
+      
+      )}
+      
 
       <ThemedView className="ml-4 mt-4 w-1/4">
         <ThemedText className="text-xl font-bold pl-5">Account</ThemedText>
       </ThemedView>
+
 
       <ThemedView className="mt-3">
         <ThemedButton className={`${componentcolor} w-4/5 h-40 rounded-[20]`}>
