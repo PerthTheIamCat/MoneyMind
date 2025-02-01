916b124996734ddadbb133d0556c48b9a5817160
SoHizz21 <sohizz1201@gmail.com>
2025-02-02 02:52:55 +0700

    set color component retire and fomat code

--- Client/app/(tabs)/index.tsx
+++ Client/app/(tabs)/index.tsx
@@ -1,53 +1,66 @@
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
-import { ThemedSafeAreaView } from "@/components/ThemedSafeAreaView";
+import { ThemedButton } from "@/components/ThemedButton";
 
-import Ionicons from "@expo/vector-icons/Ionicons";
-import AntDesign from "@expo/vector-icons/AntDesign";
-import Feather from "@expo/vector-icons/Feather";
+import Ionicons from '@expo/vector-icons/Ionicons';
+import AntDesign from '@expo/vector-icons/AntDesign';
+import Feather from '@expo/vector-icons/Feather';
+
+import { useColorScheme } from "react-native";
+import { View } from "react-native";
+
+const theme = useColorScheme();
+const componentcolor = theme === "dark" ? "!bg-[#8f8f8f]" : "!bg-[#d8d8d8]";
+const componenticon = theme === "dark" ? "#f2f2f2" : "#2f2f2f";
+
 
 export default function Index() {
-  return (
-    <ThemedSafeAreaView>
+  return(
+    <ThemedView>
 
-      <ThemedView className="mt-2 mx-5 h-10 bg-[#2f2f2f] justify-between flex-row">
-        <ThemedText className="text-#2f2f2f text-lg">HomeVeiw</ThemedText>
-        <Ionicons name="notifications-outline" size={24} color="white"/>
+      <ThemedView className={"mt-2 mx-5 h-10 justify-between flex-row"}>
+        <ThemedText className="text-#2f2f2f text-lg">HomeView</ThemedText>
+        <ThemedButton className="bg-transparent">
+          <Ionicons name="notifications-outline" size={24} color="white"/>
+        </ThemedButton>
       </ThemedView>
 
+
       <ThemedView className="ml-4 mt-4 w-1/2 flex-row">
-        <Feather name="circle" size={40} color="#f2f2f2" />
+        <Feather name="circle" size={40} color={`${componenticon}`} />
         <ThemedText className="text-xl font-bold pl-3">
           Astrex Destineth
         </ThemedText>
       </ThemedView>
 
-      <ThemedView className="mt-3">
-        <ThemedView className="w-4/5 h-40 bg-[#8f8f8f] rounded-[20]">
-          <ThemedText className="font-bold">
-            Your Monthly Save Goal
-          </ThemedText>
-          <AntDesign name="filetext1" size={50} color="#f2f2f2" className="m-3"/>
-          <ThemedText className="mx-5 text-center font-bold">
-            Let’s get started with your first 
-            retirement plan!
-          </ThemedText>
+       <ThemedView className="mt-3">
+          <ThemedButton className={`${componentcolor} h-40 w-4/5 rounded-[20]`}>
+              <ThemedView className="bg-transparent">
+                <ThemedText className="font-bold">
+                  Your Monthly Save Goal
+                </ThemedText>
+                <AntDesign name="filetext1" size={50} color={`${componenticon}`} className="m-3"/>
+                <ThemedText className="mx-5 text-center font-bold">
+                  Let’s get started with your first 
+                  retirement plan!
+                </ThemedText>
+              </ThemedView>
+          </ThemedButton>
         </ThemedView>
-      </ThemedView>
 
-      <ThemedView className="ml-4 mt-4 w-1/4">
+
+      <View className="w-full h-12 ml-10 mt-8 justify-center items-start"> 
         <ThemedText className="text-xl font-bold pl-5">Account</ThemedText>
-      </ThemedView>
+      </View>
 
-      <ThemedView className="mt-3">
-        <ThemedView className="w-4/5 h-40 bg-[#8f8f8f] rounded-[20]">
-          <AntDesign name="filetext1" size={50} color="#f2f2f2" className="m-3"/>
-          <ThemedText className="mx-5 text-center font-bold">
-            Let’s get started with your first Money Account plan!
+      <View className="bg-[#8f8f8f] w-4/5 h-40 p-8 rounded-[20] justify-center items-center">
+          <AntDesign name="filetext1" size={50} color="#f2f2f2" />
+          <ThemedText className="text-[#f2f2f2] text-center font-bold mt-4">
+            Let’s get started with your 
+            first Money Account plan!
           </ThemedText>
-        </ThemedView>
-      </ThemedView>
-      
-    </ThemedSafeAreaView>
+      </View>
+
+    </ThemedView>
   );
 }
