04e3bac9cc10952c8f323062b3bd826c5412b300
SoHizz21 <sohizz1201@gmail.com>
2025-02-01 22:03:37 +0700

    recode Homepage

--- Client/app/(tabs)/index.tsx
+++ Client/app/(tabs)/index.tsx
@@ -1,48 +1,49 @@
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
-import { ThemedButton } from "@/components/ThemedButton";
+import { ThemedSafeAreaView } from "@/components/ThemedSafeAreaView";
 
-import Ionicons from '@expo/vector-icons/Ionicons';
-import AntDesign from '@expo/vector-icons/AntDesign';
-import Feather from '@expo/vector-icons/Feather';
-
-import { View } from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import Feather from "@expo/vector-icons/Feather";
 
 export default function Index() {
-  return(
-    <ThemedView>
+  return (
+    <ThemedSafeAreaView>
 
-      <View className=" absolute top-0  bg-#f2f2f2 w-full h-10 mt-10 justify-between flex-row items-start ">
-          <ThemedText className="text-#2f2f2f text-lx pl-5 ">HomeVeiw</ThemedText>
-          <Ionicons name="notifications-outline" size={24} color="white" className="pr-5" />
-      </View>
+      <ThemedView className="mt-2 mx-5 h-10 bg-[#2f2f2f] justify-between flex-row">
+        <ThemedText className="text-#2f2f2f text-lg">HomeVeiw</ThemedText>
+        <Ionicons name="notifications-outline" size={24} color="white"/>
+      </ThemedView>
 
-      <View className="w-full justify-start ml-16 h-14 items-start flex-row"> 
+      <ThemedView className="ml-4 mt-4 w-1/2 flex-row">
         <Feather name="circle" size={40} color="#f2f2f2" />
-        <ThemedText className="text-xl font-bold pl-3 pt-2">Astrex Destineth</ThemedText>
-      </View>
+        <ThemedText className="text-xl font-bold pl-3">
+          Astrex Destineth
+        </ThemedText>
+      </ThemedView>
 
-      <View className="bg-[#8f8f8f] w-4/5 h-40 p-8 rounded-[20] justify-center items-center ">
-          <ThemedText className="text-[#f2f2f2] font-bold">Your Monthly Save Goal</ThemedText>
+      <ThemedView className="mt-3">
+        <ThemedView className="w-4/5 h-40 p-8 bg-[#8f8f8f] rounded-[20] justify-center items-start">
+          <ThemedText className="text-[#f2f2f2] font-bold">
+            Your Monthly Save Goal
+          </ThemedText>
           <AntDesign name="filetext1" size={50} color="#f2f2f2" className="m-3"/>
           <ThemedText className="text-[#f2f2f2] text-center font-bold">
-            Let’s get started with your 
-            first retirement plan!
+            Let’s get started with your first retirement plan!
           </ThemedText>
-      </View>
+        </ThemedView>
+      </ThemedView>
 
-      <View className="w-full h-12 ml-10 mt-8 justify-center items-start"> 
+      <ThemedView className="ml-4 mt-4 w-1/4 items-start">
         <ThemedText className="text-xl font-bold pl-5">Account</ThemedText>
-      </View>
+      </ThemedView>
 
-      <View className="bg-[#8f8f8f] w-4/5 h-40 p-8 rounded-[20] justify-center items-center">
-          <AntDesign name="filetext1" size={50} color="#f2f2f2" />
-          <ThemedText className="text-[#f2f2f2] text-center font-bold mt-4">
-            Let’s get started with your 
-            first Money Account plan!
-          </ThemedText>
-      </View>
-
-    </ThemedView>
+      <ThemedView className="bg-[#8f8f8f] w-4/5 h-40 p-8 rounded-[20] justify-center items-center">
+        <AntDesign name="filetext1" size={50} color="#f2f2f2" />
+        <ThemedText className="text-[#f2f2f2] text-center font-bold mt-4">
+          Let’s get started with your first Money Account plan!
+        </ThemedText>
+      </ThemedView>
+    </ThemedSafeAreaView>
   );
 }
