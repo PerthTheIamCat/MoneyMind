fedfe2e4088d0a60e89365304486aa161456771b
Phoom_PS <Phoom289164@gmail.com>
2025-02-02 13:25:13 +0700

    add view card

--- Client/app/(tabs)/transaction.tsx
+++ Client/app/(tabs)/transaction.tsx
@@ -6,9 +6,11 @@ import Ionicons from "@expo/vector-icons/Ionicons";
 import { ThemedButton } from "@/components/ThemedButton";
 import AntDesign from "@expo/vector-icons/AntDesign";
 import { View, Text } from "react-native";
-import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import { ThemedScrollView } from "@/components/ThemedScrollView";
 
 export default function Index() {
+  
   return (
     <ThemedSafeAreaView>
       <ThemedView className="flex-row items-center justify-between bg-red-500 px-4">
@@ -18,7 +20,7 @@ export default function Index() {
           style={{
             width: 79,
             height: 70,
-            marginTop: "10%",
+            marginTop: "2%",
             marginLeft: "5%",
           }}
         />
@@ -26,24 +28,31 @@ export default function Index() {
           name="notifications-outline"
           size={32}
           color="black"
-          style={{ alignSelf: "center", marginTop: "10%" }}
+          style={{ alignSelf: "center", marginTop: "5%" , marginRight: "5%" }}
         />
       </ThemedView>
       <ThemedView className="!items-start pl-[10%] pt-[2%] bg-yellow-700">
         <ThemedText className=" text-[18px]">Connected</ThemedText>
         <ThemedText className="font-bold text-[24px]">Accounts</ThemedText>
       </ThemedView>
-      <ThemedView className="bg-blue-400 h-[170px] !items-start">
-        <View className="flex flex-row justify-center items-center rounded-xl -rotate-90  w-[150px] h-[40px] bg-gray-400 -ml-5 active:scale-105">
-          <AntDesign name="plus" size={20} color="black" className="" />
+      <ThemedView className="bg-blue-400 h-[150px] !items-center flex flex-row ">
+        <View className="flex flex-row justify-center items-center rounded-xl -rotate-90  w-[125px] h-[40px] bg-gray-400 -ml-2 active:scale-105">
+          <AntDesign name="plus" size={20} color="black" />
           <Text>Add Account</Text>
         </View>
+        <ThemedView className="bg-yellow-600 rounded-[15px] w-9/12 -ml-9">
+          <View className="w-96 h-40">
+
+          </View>
+        </ThemedView>
       </ThemedView>
       <ThemedView className="flex-row items-center bg-red-400 justify-between px-4">
-        <ThemedText className="text-[18px] pl-[5%] font-bold">Transaction</ThemedText>
-        <View className="font-bold flex flex-row">
-          <Text className="font-bold items-center mt-1">All</Text>
-          <MaterialIcons name="arrow-drop-down" size={24} color="black" />
+        <ThemedText className="text-[20px] pl-[5%] font-bold">
+          Transaction
+        </ThemedText>
+        <View className="font-bold flex flex-row mr-6">
+          <Text className="font-bold items-center mt-1 text-[18px]">All</Text>
+          <MaterialIcons name="arrow-drop-down" size={26} color="black" className="mt-1"/>
         </View>
       </ThemedView>
     </ThemedSafeAreaView>
