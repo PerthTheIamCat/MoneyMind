4751163ec9f75904b071816b10c01c827b0a3e42
Maruki119 <93820527+Maruki119@users.noreply.github.com>
2025-02-02 18:04:04 +0700

    Server: Add OTP auth and add sendEmail function in sendEmail.js

--- Server/routes/auth.js
+++ Server/routes/auth.js
@@ -2,9 +2,11 @@ const express = require('express')  // Import express
 const router = express.Router()     // Create express app
 const bcrypt = require('bcrypt')    // Import bcrypt for password hashing
 const jwt = require('jsonwebtoken') // Import jsonwebtoken for token generation
+const otpGenerator = require('otp-generator') // Import otp-generator for OTP generation
 require('dotenv').config();        // Import dotenv for environment variables
 
 const db = require('./db');
+const {sendEmail} = require('./sendEmail')
 
 router.use(express.json());
 router.use(express.urlencoded({ extended: false }))
@@ -45,7 +47,7 @@ const jwtAccessTokenGenrate = (UserID, username, email) => {
     const accessToken = jwt.sign(
         { UserID, username, email },
         process.env.ACCESS_TOKEN_SECRET, 
-        { expiresIn: '20m', algorithm: 'HS256' })
+        { expiresIn: '10s', algorithm: 'HS256' })
     
     return accessToken
 }
@@ -74,9 +76,9 @@ const jwtValidate = (req, res, next) => {
 };
 
 router.post('/register', (req, res) => {
-    const { username, email, password, password2 } = req.body
+    const { username, email, password, password2, otp} = req.body
     
-    const emailRegex = /^[^\s@]+@[^\s@]+\.com$/
+    const emailRegex = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/
     if (!emailRegex.test(`${email}`)) {
         return res.status(400).json({message: 'Invalid email format', success: false})
     }
@@ -103,26 +105,46 @@ router.post('/register', (req, res) => {
                 return res.status(400).json({ message: 'Passwords do not match', success: false});
             }
 
-            bcrypt.hash(password, 10, (err, hash) => {
+            db.query('SELECT * FROM otp WHERE email = ?', [email], async (err, result) => {
                 if (err) {
-                    console.error('Error hashing password:', err)
-                    return res.status(500).json({ message: 'Password hashing failed', error: err.message, success: false});
+                    return res.status(500).json({ message: 'Database query failed', error: err.message, success: false});
                 }
 
-                db.query('INSERT INTO users (username, email, password) VALUES (?, ?, ?)', [username, email, hash], async (err, result) => {
-                    if (err) {
-                        return res.status(500).json({ message: 'Database query failed', error: err.message, success: false});
+                const otpData = result[0]
+
+                // console.log(otp)
+                // console.log(otpData.otp_code)
+
+                if (otpData.otp_code === otp) {
+                    if (otpData.expires_at < new Date()) {
+                        return res.status(400).json({ message: 'OTP expired', success: false });
+                    }else{
+                        console.log('OTP verified')
+                        bcrypt.hash(password, 10, (err, hash) => {
+                            if (err) {
+                                console.error('Error hashing password:', err)
+                                return res.status(500).json({ message: 'Password hashing failed', error: err.message, success: false});
+                            }
+            
+                            db.query('INSERT INTO users (username, email, password) VALUES (?, ?, ?)', [username, email, hash], async (err, result) => {
+                                if (err) {
+                                    return res.status(500).json({ message: 'Database query failed', error: err.message, success: false});
+                                }
+                                
+                                const UserID = await getUserIDbyusername(username)
+            
+                                console.log(UserID, username, email)
+            
+                                const accessToken = jwtAccessTokenGenrate(UserID, username, email)
+            
+                                console.log('User created')
+                                return res.status(201).json({ accessToken, message: 'User created', success: true})
+                            })
+                        })
                     }
-                    
-                    const UserID = await getUserIDbyusername(username)
-
-                    console.log(UserID, username, email)
-
-                    const accessToken = jwtAccessTokenGenrate(UserID, username, email)
-
-                    console.log('User created')
-                    return res.status(201).json({ accessToken, message: 'User created', success: true})
-                })
+                }else{
+                    return res.status(400).json({ message: 'Invalid OTP', success: false });
+                }
             })
         })
     })
@@ -133,7 +155,7 @@ router.post('/login', (req, res) => {
     const { input, password } = req.body
     let userOrEmail = "username"
 
-    const emailRegex = /^[^\s@]+@[^\s@]+\.com$/
+    const emailRegex = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/
     if (emailRegex.test(`${input}`)){
         userOrEmail = "email"
     }
@@ -167,6 +189,71 @@ router.post('/login', (req, res) => {
             return res.status(200).json({ accessToken, message: 'Login successful', success: true });
         });
     });
+})
+
+router.post('/otpSend', (req, res) => {
+    const { email } = req.body
+
+    const emailRegex = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/
+    if (!emailRegex.test(email)) {
+        return res.status(400).json({ message: 'Invalid email format', success: false })
+    }
+
+    const otp = otpGenerator.generate(6, {
+        digits: true,
+        lowerCaseAlphabets: false,
+        upperCaseAlphabets: false,
+        specialChars: false
+    })
+
+    const created_at = new Date()
+    const expires_at = new Date(Date.now() + 5 * 60 * 1000)
+
+    db.query('SELECT * FROM otp WHERE email = ?', [email], async (err, result) => {
+        if (err) {
+            return res.status(500).json({ message: 'Database query failed', error: err.message, success: false })
+        }
+
+        try {
+            const subject = 'Your OTP Code'
+            const text = `Your OTP is: ${otp}.`
+            const html = `<p>Your OTP is: <strong>${otp}</strong></p>`
+
+            const emailResult = await sendEmail({ email, subject, text, html })
+            console.log(emailResult)
+
+            if (emailResult.success) {
+                if (result.length > 0) {
+                    db.query(
+                        'UPDATE otp SET otp_code = ?, created_at = ?, expires_at = ?, is_used = 0 WHERE email = ?', [otp, created_at, expires_at, email],
+                        (err) => {
+                            if (err) {
+                                return res.status(500).json({ message: 'Database update failed', error: err.message, success: false })
+                            }
+
+                            return res.status(200).json({ message: 'OTP resent successfully', success: true })
+                        }
+                    )
+                } else {
+                    db.query(
+                        'INSERT INTO otp (email, otp_code, created_at, expires_at) VALUES (?, ?, ?, ?)', [email, otp, created_at, expires_at],
+                        (err) => {
+                            if (err) {
+                                return res.status(500).json({ message: 'Database insert failed', error: err.message, success: false })
+                            }
+
+                            return res.status(200).json({ message: 'OTP sent successfully', success: true })
+                        }
+                    )
+                }
+            } else {
+                return res.status(500).json({ message: 'Failed to send OTP email', success: false })
+            }
+        } catch (error) {
+            console.error('Error sending OTP email:', error)
+            return res.status(500).json({ message: 'Failed to send OTP email', error: error.message, success: false })
+        }
+    })
 })
 
 module.exports = {
