476de4116e7eb2e3b8cfa2cbfd8a37799b33c1c0
santamonika-111 <127444524+santamonika-111@users.noreply.github.com>
2025-02-07 13:57:16 +0700

    #Build Header

--- Client/app/(tabs)/splitpay.tsx
+++ Client/app/(tabs)/splitpay.tsx
@@ -1,13 +1,45 @@
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
 import { ThemedSafeAreaView } from '@/components/ThemedSafeAreaView';
+import { router } from "expo-router";
+import { Image } from "expo-image";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
-export default function Index() {
+
+export default function splitpay() {
   return (
     <ThemedSafeAreaView>
+      {/*Header*/}
+      <ThemedView className="flex-row items-center justify-between px-4">
+      <Image
+          className="ml-[10%]"
+          source={require("@/assets/logos/LOGO.png")}
+          style={{
+            width: 79,
+            height: 70,
+            marginTop: "2%",
+            marginLeft: "5%",
+          }}
+        />
+        <Ionicons
+          onPress={() => router.push("/Add_Transaction")}
+          name="notifications-outline"
+          size={32}
+          color="black"
+          style={{ alignSelf: "center", marginTop: "5%", marginRight: "5%" }}
+        />
+      </ThemedView>
+
+      {/* Decision Menu */}
       <ThemedView >
-        <ThemedText>Edit app/splitpay.tsx to edit this screen.</ThemedText>
-        <ThemedText> This is splitpay page </ThemedText>
+        <ThemedText>  </ThemedText>
+        <ThemedText>  </ThemedText>
+      </ThemedView>
+      
+      {/* Add Account Box */}
+      <ThemedView >
+        <ThemedText>  </ThemedText>
+        <ThemedText>  </ThemedText>
       </ThemedView>
     </ThemedSafeAreaView>
   );
