41bd3950d04ef41275f73de37721f4ecdd402b6a
Phoom_PS <Phoom289164@gmail.com>
2025-02-09 18:53:44 +0700

    update clean code

--- Client/app/(tabs)/transaction.tsx
+++ Client/app/(tabs)/transaction.tsx
@@ -14,7 +14,7 @@ import { useColorScheme } from "react-native";
 import { UserContext } from "@/hooks/conText/UserContext";
 import { useContext } from "react";
 import { useState, useEffect } from "react";
-import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 import { TouchableWithoutFeedback } from "react-native";
 import { Animated, Easing } from "react-native";
 
@@ -103,12 +103,20 @@ const transactions: Transaction[] = [
   },
 ];
 
-const TransactionItem = ({ transaction, theme }: { transaction: Transaction, theme: string | null }) => {
+const TransactionItem = ({
+  transaction,
+  theme,
+}: {
+  transaction: Transaction;
+  theme: string | null;
+}) => {
   const componentcolor = theme === "dark" ? "!bg-[#181818]" : "!bg-[#d8d8d8]";
   const componenticon = theme === "dark" ? "#f2f2f2" : "#2f2f2f";
 
   return (
-    <View className={`flex-row items-center justify-center w-10/12 ${componentcolor} p-4 rounded-lg mb-2 shadow-md`}>
+    <View
+      className={`flex-row items-center justify-center w-10/12 ${componentcolor} p-4 rounded-lg mb-2 shadow-md`}
+    >
       <Image
         source={transaction.logo}
         style={{ width: 40, height: 40, borderRadius: 20, marginRight: 16 }}
@@ -118,19 +126,27 @@ const TransactionItem = ({ transaction, theme }: { transaction: Transaction, the
         <ThemedText className={`font-bold text-lg `}>
           {transaction.category}
         </ThemedText>
-        <ThemedText className={``}>
-          {transaction.description}
-        </ThemedText>
+        <ThemedText className={``}>{transaction.description}</ThemedText>
       </View>
 
-      <Text className={`font-bold text-[16px] ${transaction.transaction_type === "income" ? "text-green-500" : "text-red-500"}`}>
+      <Text
+        className={`font-bold text-[16px] ${
+          transaction.transaction_type === "income"
+            ? "text-green-500"
+            : "text-red-500"
+        }`}
+      >
         {transaction.amount}
       </Text>
-      <Entypo name="dots-three-vertical" size={20} color={componenticon} className="ml-2" />
+      <Entypo
+        name="dots-three-vertical"
+        size={20}
+        color={componenticon}
+        className="ml-2"
+      />
     </View>
   );
 };
-
 
 export default function Index() {
   const { bank } = useContext(UserContext);
@@ -156,7 +172,7 @@ export default function Index() {
   }, [isOverlayVisible]);
 
   return (
-    <>   
+    <>
       <ThemedSafeAreaView>
         <ThemedScrollView>
           <ThemedView
@@ -189,7 +205,9 @@ export default function Index() {
             <ThemedText className="font-bold text-[24px]">Accounts</ThemedText>
           </ThemedView>
           <ThemedView className="bg-[E5E5E5] h-[154px] !items-center flex flex-row ">
-            <View className={`flex flex-row justify-center items-center rounded-xl -rotate-90  w-[125px] h-[45px] ${componentcolor} -ml-2 active:scale-105`}>
+            <View
+              className={`flex flex-row justify-center items-center rounded-xl -rotate-90  w-[125px] h-[45px] ${componentcolor} -ml-2 active:scale-105`}
+            >
               <AntDesign name="plus" size={20} color={`${componenticon}`} />
               <ThemedText className="font-bold">Add Account</ThemedText>
             </View>
@@ -197,19 +215,19 @@ export default function Index() {
               horizontal={true}
               className=" bg-[E5E5E5] pl-2 rounded-tl-[15px] rounded-bl-[15px] w-5/6 -ml-9"
             >
-          <View className="mt-0.5 mb-1 flex-row space-x-1">
-            {bank?.map((account) => (
-              <ThemedCard
-                name={account.account_name}
-                color={account.color_code}
-                balance={account.balance.toString()}
-                mode="small"
-                onEdit={() => {}}
-                key={account.id}
-                // image={account.icon_id}
-                className="!items-center !justify-center w-32 h-32 bg-[#fefefe] rounded-lg"
-              />
-            ))}
+              <View className="mt-0.5 mb-1 flex-row space-x-1">
+                {bank?.map((account) => (
+                  <ThemedCard
+                    name={account.account_name}
+                    color={account.color_code}
+                    balance={account.balance.toString()}
+                    mode="small"
+                    onEdit={() => {}}
+                    key={account.id}
+                    // image={account.icon_id}
+                    className="!items-center !justify-center w-32 h-32 bg-[#fefefe] rounded-lg"
+                  />
+                ))}
               </View>
             </ThemedScrollView>
           </ThemedView>
@@ -241,7 +259,7 @@ export default function Index() {
                         {transaction.date}
                       </ThemedText>
                     )}
-                    <TransactionItem transaction={transaction} theme={theme}/>
+                    <TransactionItem transaction={transaction} theme={theme} />
                   </View>
                 );
               })}
@@ -251,46 +269,64 @@ export default function Index() {
       </ThemedSafeAreaView>
 
       {isOverlayVisible && (
-        <TouchableWithoutFeedback onPress={() => {
-          // เริ่มอนิเมชันเลื่อนลง
-          Animated.timing(slideAnim, {
-            toValue: 300, // เลื่อนลง
-            duration: 300,
-            easing: Easing.ease,
-            useNativeDriver: true,
-          }).start(() => {
-            // เมื่ออนิเมชันเลื่อนลงเสร็จแล้ว
-            setIsOverlayVisible(false);
-          });
-        }}
-      >
-        <View className="absolute inset-0 bg-[#00000055] flex items-center justify-end pb-16">
-        <Animated.View
-        style={{
-          transform: [{ translateY: slideAnim }], // ใช้ slideAnim เพื่อเลื่อนขึ้น
-          width: '80%',
-        }}
-        className="p-6 rounded-lg"
-      >
-         <ThemedView className=" p-6 rounded-lg w-full ">
-            <ThemedText className="text-3xl font-bold ">Insert Type</ThemedText>
-            <View className="flex flex-row gap-6 mt-2 rounded-lg">
-              <View className={`${componentcolor}  p-1 rounded-lg mx-2`}>
-                <MaterialCommunityIcons name="notebook" size={54} color="black" className="bg-[#AACC00] m-2 mr-11 rounded-lg"/>
-                <ThemedText className="font-bold">Add By Yourself</ThemedText>
-              </View>
-              <View className={`${componentcolor}  p-1 rounded-lg mx-2`}>
-                <Ionicons name="camera-sharp" size={54} color="black" className="bg-[#AACC00] w-fit m-2 mr-11 rounded-lg"/>
-                <ThemedText className="font-bold">Add By Camera</ThemedText>
-              </View>
-            </View>
-            </ThemedView>
-          </Animated.View>
-        </View>
+        <TouchableWithoutFeedback
+          onPress={() => {
+            // เริ่มอนิเมชันเลื่อนลง
+            Animated.timing(slideAnim, {
+              toValue: 300, // เลื่อนลง
+              duration: 300,
+              easing: Easing.ease,
+              useNativeDriver: true,
+            }).start(() => {
+              // เมื่ออนิเมชันเลื่อนลงเสร็จแล้ว
+              setIsOverlayVisible(false);
+            });
+          }}
+        >
+          <View className="absolute inset-0 bg-[#00000055] flex items-center justify-end pb-16">
+            <Animated.View
+              style={{
+                transform: [{ translateY: slideAnim }], // ใช้ slideAnim เพื่อเลื่อนขึ้น
+                width: "80%",
+              }}
+              className="p-6 rounded-lg"
+            >
+              <ThemedView className=" p-6 rounded-lg w-full ">
+                <ThemedText className="text-3xl font-bold ">
+                  Insert Type
+                </ThemedText>
+                <View className="flex flex-row gap-6 mt-2 rounded-lg">
+                  <View className={`${componentcolor}  p-1 rounded-lg mx-2`}>
+                    <MaterialCommunityIcons
+                      name="notebook"
+                      size={54}
+                      color="black"
+                      className="bg-[#AACC00] m-2 mr-11 rounded-lg"
+                    />
+                    <ThemedText className="font-bold">
+                      Add By Yourself
+                    </ThemedText>
+                  </View>
+                  <View className={`${componentcolor}  p-1 rounded-lg mx-2`}>
+                    <Ionicons
+                      name="camera-sharp"
+                      size={54}
+                      color="black"
+                      className="bg-[#AACC00] w-fit m-2 mr-11 rounded-lg"
+                    />
+                    <ThemedText className="font-bold">Add By Camera</ThemedText>
+                  </View>
+                </View>
+              </ThemedView>
+            </Animated.View>
+          </View>
         </TouchableWithoutFeedback>
       )}
 
-      <Pressable onPress={() => setIsOverlayVisible(true)} className="!absolute bottom-6 right-6 bg-transparent">
+      <Pressable
+        onPress={() => setIsOverlayVisible(true)}
+        className="!absolute bottom-6 right-6 bg-transparent"
+      >
         <View className="!items-center !justify-center bg-[#aacc00] w-16 h-16 rounded-full ">
           <AntDesign name="plus" size={32} color="#ffffff" />
         </View>
