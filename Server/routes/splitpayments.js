5fdb3f690f8d165ba4ece725e3baeb5c27699e6b
Maruki119 <93820527+Maruki119@users.noreply.github.com>
2025-02-09 18:28:37 +0700

    Server: update .post splitpayments.js

--- Server/routes/splitpayments.js
+++ Server/routes/splitpayments.js
@@ -28,49 +28,36 @@ router.post('/create', jwtValidate, (req, res) => {
     }
 
     db.query(
-        'SELECT * FROM bankaccounts WHERE id = ? AND user_id = ?', [account_id, user_id], (err, result) => {
+        'SELECT b.balance, SUM(s.amount_allocated) AS sumAmount FROM bankaccounts b LEFT JOIN splitpayments s ON b.id = s.account_id WHERE b.id = ? and b.user_id = ? GROUP BY b.id, b.balance', [account_id, user_id], (err, result) => {
             if (err) {
                 return res.status(500).json({ message: 'Database query failed', error: err.message, success: false });
             }
 
             if (result.length === 0) {
-                return res.status(403).json({ message: 'Unauthorized user or account not found', success: false });
+                return res.status(403).json({ message: 'Unauthorized user or Split Pay not found', success: false });
             }
 
-            let bankData = result[0]
-            console.log(bankData)
+            let sumSplitpay = result[0]
+            console.log(sumSplitpay)
 
-            let remaining = bankData.remaining - amount_allocated
+            let remaining = sumSplitpay.balance - sumSplitpay.sumAmount
+            console.log(remaining)
 
-            if (remaining < 0) {
+            if (remaining - amount_allocated < 0) {
                 return res.status(400).json({ message: 'Your Bank balance remaining does not enough', success: false });
             }
-            
+
             db.query(
-                'UPDATE bankaccounts SET remaining = ? WHERE id = ?', [remaining, account_id], (err, bankResult) => {
+                'INSERT INTO splitpayments (account_id, split_name, amount_allocated, remaining_balance, color_code, icon_id) VALUES (?, ?, ?, ?, ?, ?)',
+                [account_id, split_name, amount_allocated, remaining_balance, color_code, icon_id || null],
+                (err, splitpeyResult) => {
                     if (err) {
                         return res.status(500).json({ message: 'Database query failed', error: err.message, success: false });
                     }
-        
-                    if (bankResult.length === 0) {
-                        return res.status(404).json({ message: 'Bank Account not found', success: false });
-                    }
-        
-                    db.query(
-                        'INSERT INTO splitpayments (account_id, split_name, amount_allocated, remaining_balance, color_code, icon_id) VALUES (?, ?, ?, ?, ?, ?)',
-                        [account_id, split_name, amount_allocated, remaining_balance, color_code, icon_id || null],
-                        (err, splitpeyResult) => {
-                            if (err) {
-                                return res.status(500).json({ message: 'Database query failed', error: err.message, success: false });
-                            }
-
-                            console.log('Splitpayment Created')
-                            return res.status(201).json({splitpeyResult, message: 'Splitpayment Created', success: true })
-                        }
-                    )
+                    console.log('Splitpayment Created')
+                    return res.status(201).json({splitpeyResult, message: 'Splitpayment Created', success: true })
                 }
             )
-            
         } 
     )
 })
@@ -89,6 +76,28 @@ router.get('/:id', jwtValidate, (req, res) => {
             return res.status(200).json({result, success: true});
         }
     )
+})
+
+router.put('/:id', jwtValidate, (req, res) => {
+    const bankID = req.params.id;
+
+    db.query(
+        'SELECT * FROM bankaccounts WHERE id = ? AND user_id = ?', [account_id, user_id], (err, result) => {
+            if (err) {
+                return res.status(500).json({ message: 'Database query failed', error: err.message, success: false });
+            }
+
+            if (result.length === 0) {
+                return res.status(403).json({ message: 'Unauthorized user or account not found', success: false });
+            }
+
+            
+        }
+    )
+})
+
+router.delete('/:id', jwtValidate, (req, res) => {
+    
 })
 
 
