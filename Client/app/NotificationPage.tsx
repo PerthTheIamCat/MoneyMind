0a4a0edcee6696d0b9c8876b3ba6d7a43e6570dd
SoHizz21 <sohizz1201@gmail.com>
2025-02-14 02:05:27 +0700

    add data Check

--- Client/app/NotificationPage.tsx
+++ Client/app/NotificationPage.tsx
@@ -9,16 +9,17 @@ export default function Index() {
     const componentcolor = theme === "dark" ? "!bg-[#181818]" : "!bg-[#d8d8d8]";
 
     const [data, setData] = useState([
-        { mode: "red" ,Header: "Topic1", Description: "H3" },
-        { mode: "yellow" ,Header: "Topic2", Description: "H4" },
-        { mode: "green" ,Header: "Topic3", Description: "H5" },
-        { mode: "red" ,Header: "Topic4", Description: "H6" },
-        { mode: "red" ,Header: "Topic5", Description: "H6" },
+        { mode: "red" ,Header: "Monthly Summary", Description: "You didn't save enough money last month try to be better next month." },
+        { mode: "green" ,Header: "Monthly Summary", Description: "You save enough money last month keep it up in next month." },
+        { mode: "red" ,Header: "Nearly out of money", Description: "Your funds are running low. Spend wisely before it all slips away." },
+        { mode: "red" ,Header: "Out of money", Description: "Your funds are depleted. NOw is th time to rely on practice and careful planning to start anew.  " },
+        { mode: "yellow" ,Header: "New device logged in", Description: "New device logged in on 11/01/2025 11:10 . If this not you please go to setting and change your password" },
+        { mode: "yellow" ,Header: "Password changed", Description: "Password change successfully." },
     ]);
 
     return (
         
-        <ThemedView className={`${componentcolor} h-ful justify-center items-start`}>
+        <ThemedView className={`${componentcolor} h-full w-full items-start`}>
             <ListNotification data={data} />
         </ThemedView>
     );
