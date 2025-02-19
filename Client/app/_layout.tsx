4456addfe659b9a6ef32d9bcff148c5efb1654a8
PerthTheIamCat <thongcum2546@gmail.com>
2025-02-20 02:33:01 +0700

    Add Retire form screen and update context for retirement savings

--- Client/app/_layout.tsx
+++ Client/app/_layout.tsx
@@ -117,7 +117,7 @@ export default function RootLayout() {
               <Stack.Screen name="Edit_Transaction" options={{ headerTitle: "Edit Transaction" , presentation: "modal" }} />
               <Stack.Screen name="Edit_Account" options={{ headerTitle: "Edit Account" , presentation: "modal" }} />
               <Stack.Screen name="Month_Summary" options={{ headerTitle: "Monthly Summary" , presentation: "modal" }} />
-
+              <Stack.Screen name="Retire_form" options={{ headerTitle: "Retire" , presentation: "modal" }} />
 
             </Stack>
             <StatusBar style="auto" />
