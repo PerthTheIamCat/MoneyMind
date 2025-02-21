0f49f1853b3552b9f174f42028fb13421524137a
SoHizz21 <sohizz1201@gmail.com>
2025-02-21 09:46:02 +0700

    sort card transaction Page

--- Client/app/(tabs)/transaction.tsx
+++ Client/app/(tabs)/transaction.tsx
@@ -200,7 +200,10 @@ export default function Index() {
             className=" bg-[E5E5E5] pl-2 rounded-tl-[15px] rounded-bl-[15px] w-5/6 -ml-9"
           >
             <View className="mt-0.5 mb-1 flex-row space-x-1">
-              {bank?.map((account, index) => (
+              {bank
+              ?.slice()
+              .sort((a,b)=> a.id-b.id)
+              .map((account, index) => (
                 <ThemedCard
                   CardID={account.id}
                   name={account.account_name}
