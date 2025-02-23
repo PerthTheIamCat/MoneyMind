e2aa631ec6e86685a63470c3d303049cc18b8905
Maruki119 <93820527+Maruki119@users.noreply.github.com>
2025-02-23 15:29:49 +0700

    Server: modify db.js change createConnection to createPool

--- Server/routes/db.js
+++ Server/routes/db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql')
 const fs = require('fs')
 require('dotenv').config()
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS, //Enter MySQL password in .env file
@@ -14,16 +14,13 @@ const db = mysql.createConnection({
     }
 })
 
-db.connect((err) => {
-    try{
-        if (err) {
-            throw err
-        }
-        console.log('Connected to database')
-    }catch(err){
-        console.error('Error connecting to database')
+db.getConnection((err, connection) => {
+    if (err) {
+        console.error('Error connecting to database:', err);
+        return;
     }
-
-})
+    console.log('Connected to database');
+    connection.release(); // ปล่อย connection กลับไปที่ pool
+});
 
 module.exports = db;
