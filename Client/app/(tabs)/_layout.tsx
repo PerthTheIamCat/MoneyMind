c56a292b769520d02cbfb22c5d9d93c3eb2c94bf
Aust9ny <tars222333@gmail.com>
2025-03-07 19:17:00 +0700

    new navbar with some animation

--- Client/app/(tabs)/_layout.tsx
+++ Client/app/(tabs)/_layout.tsx
@@ -1,70 +1,51 @@
-import { Tabs } from "expo-router";
-import { useColorScheme } from "react-native";
-import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Tabs } from 'expo-router'
+import TabBar from "@/components/Tabbar"
 
-export default function TabLayout() {
-  // const { theme } = useTheme();
-  const theme = useColorScheme();
-  // console.log("theme: ",theme);
+const _layout = () => {
   return (
     <Tabs
-      screenOptions={{
-        headerShown: false,
-        tabBarActiveTintColor: theme === "dark" ? "#AACC00" : "#007F5F",
-        tabBarInactiveTintColor: theme === "dark" ? "#F2F2F2" : "#2F2F2F",
-        animation: "none",
-        tabBarStyle: {
-          backgroundColor: theme === "dark" ? "#2F2F2F" : "#F2F2F2",
-          borderTopColor: theme === "dark" ? "#F2F2F2" : "#2F2F2F",
-          borderTopWidth: 1,
-        },
-      }}
+        tabBar={props=> <TabBar {...props} />}
+        screenOptions={
+           {
+            headerShown: false,
+            tabBarShowLabel: false,
+           }
+        }
     >
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: "Home",
-          tabBarIcon: ({ color }) => (
-            <FontAwesome5 name="home" size={24} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="splitpay"
-        options={{
-          title: "SplitPay",
-          tabBarIcon: ({ color }) => (
-            <FontAwesome5 name="handshake" size={24} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="transaction"
-        options={{
-          title: "Transaction",
-          tabBarIcon: ({ color }) => (
-            <FontAwesome5 name="exchange-alt" size={24} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="retire"
-        options={{
-          title: "Retire",
-          tabBarIcon: ({ color }) => (
-            <FontAwesome5 name="piggy-bank" size={24} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="me"
-        options={{
-          title: "Me",
-          tabBarIcon: ({ color }) => (
-            <FontAwesome5 name="home" size={24} color={color} />
-          ),
-        }}
-      />
+        <Tabs.Screen
+            name="index"
+            options={{
+                title: "Home"
+            }}
+        />
+        <Tabs.Screen
+            name="splitpay"
+            options={{
+                title: "SplitPay"
+            }}
+        />
+        <Tabs.Screen
+            name="transaction"
+            options={{
+                title: "transaction"
+            }}
+        />
+        <Tabs.Screen
+            name="retire"
+            options={{
+                title: "Retire"
+            }}
+        />
+           <Tabs.Screen
+            name="me"
+            options={{
+                title: "Me"
+            }}
+        />
     </Tabs>
-  );
+  )
 }
+
+export default _layout
