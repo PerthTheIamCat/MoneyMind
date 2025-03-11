3cad5b73f526bfcd42204bb67696a03b22080d40
SoHizz21 <sohizz1201@gmail.com>
2025-03-11 17:40:22 +0700

    Welcomepage remake

--- Client/app/Welcome.tsx
+++ Client/app/Welcome.tsx
@@ -5,42 +5,39 @@ import { Image } from "expo-image";
 import { router } from "expo-router";
 import React, { useContext,useEffect,useRef } from "react";
 import { AuthContext } from "@/hooks/conText/AuthContext";
-import { ThemedSafeAreaView } from "@/components/ThemedSafeAreaView";
 import { ThemedScrollView } from "@/components/ThemedScrollView";
 import { useWindowDimensions,  Animated,useAnimatedValue } from "react-native";
-import { View } from "react-native";
+
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+SplashScreen.preventAutoHideAsync()
+
 
 export default function Welcome() {
+
+  const [fontsLoaded] = useFonts({
+    "PlusJakartaSans": require("../assets/fonts/PlusJakartaSans-Italic.ttf"),
+    "Prompt-Regular": require("../assets/fonts/Prompt-Regular.ttf"),
+    "Poppins": require("../assets/fonts/Poppins/Poppins-ExtraBold.ttf"),
+    "PlusJakartaSans-wght": require("../assets/fonts/PlusJakartaSans-wght.ttf"),
+  });
+
   const auth = useContext(AuthContext);
   const { width } = useWindowDimensions();
-  
-  const isLargeScreen = width > 500; // ตรวจสอบว่าจอใหญ่กว่า 600px หรือไม่
+  const isLargeScreen = width > 500; // ตรวจสอบว่าจอใหญ่กว่า 600px หรืrrอไม่
   
   const fadeAnim = useAnimatedValue(0);
-
-  const translateY = useRef(new Animated.Value(200)).current;
-  const translateX = useRef(new Animated.Value(200)).current;
   const scaleAnim = useRef(new Animated.Value(0)).current;
-  const ab = useRef(new Animated.Value(0)).current;
+  const bounding = useRef(new Animated.Value(0)).current;
+  const trany= useRef(new Animated.Value(500)).current;
 
   useEffect(() => {
     Animated.timing(fadeAnim, {
       toValue: 1,
       duration: 2000,
+      delay: 2500, 
       useNativeDriver: true,
       
-    }).start();
-
-    Animated.timing(translateY, {
-      toValue: 0, // ขยับไปตำแหน่งเริ่มต้น (0)
-      duration: 2000,
-      useNativeDriver: true,
-    }).start();
-  
-    Animated.timing(translateX, {
-      toValue: 0, // ขยับไปตำแหน่งเริ่มต้น (0)
-      duration: 2000,
-      useNativeDriver: true,
     }).start();
 
     Animated.spring(scaleAnim, {
@@ -51,16 +48,28 @@ export default function Welcome() {
       useNativeDriver: true,
     }).start();
     
-    Animated.timing(ab, {
-      toValue: 100,
+    Animated.timing(bounding, {
+      toValue: 5,
       duration: 1000, // ค่ามากขึ้น = ลดแรงเด้ง ทำให้ขึ้นช้าลง 
       delay: 1500, 
+      useNativeDriver: true,
+    }).start();
+
+    Animated.timing(trany, {
+      toValue: 0,
+      delay: 2000, 
       useNativeDriver: true,
     }).start();
 
   
   }, []);
 
+  const bounding1 = bounding;
+  const bounding2 = bounding.interpolate({
+    inputRange: [0,3], // เริ่มช้ากว่า
+    outputRange: [0,5], // ค้างไว้ที่ 0 ก่อน แล้วค่อยโตขึ้น
+    extrapolate: "clamp",
+  });
 
   return (
       <ThemedScrollView
@@ -71,62 +80,74 @@ export default function Welcome() {
           paddingBottom: 5,
         }}
         showsVerticalScrollIndicator={false}
-      >
-        <Animated.View className="bg-green-700 w-10  h-10 absolute top-44 rounded-full" style={{transform: [{ scale: ab }] }} >
-          
+        >
+        <Animated.View className="bg-green-200 w-96  h-96 absolute top-[10%] rounded-full" style={{transform: [{ scale: bounding2 }] }} >
         </Animated.View>
-        <ThemedView className="items-center w-full  bg-transparent">
-          <Animated.View style={{ opacity: fadeAnim ,transform: [{ scale: scaleAnim }] }}>
+        <Animated.View className="bg-green-700  w-96  h-96  absolute top-[10%] rounded-full" style={{transform: [{ scale: bounding1 }] }} >
+        </Animated.View>
+        <Animated.View className="bg-slate-200  w-[100%]  h-[60%]  absolute bottom-0 rounded-t-[10%]" style={{transform:[{translateY:trany}]}} >
+        </Animated.View>
+          
+        <ThemedView className="items-center w-full  absolute top-[5%] bg-transparent">
+          <Animated.View style={{ transform: [{ scale: scaleAnim }] }}>
             <Image
               source={require("@/assets/logos/LOGO.png")}
               style={{
                 width: isLargeScreen ? 400 : width < 380 ? 200 : 250,
                 height: isLargeScreen ? 500 : width < 380 ? 300 : 350,
               }}
-              contentFit="contain"
+              contentFit="contain" 
               />
           </Animated.View>
-          <ThemedView className="!items-start w-full px-6 text-center pl-10 bg-transparent">
-            <ThemedText
-              className="text-5xl mb-10 !text-[#fff]"
-              style={{ fontSize: isLargeScreen ? 56 : width < 380 ? 32 : 46 }}
-            >
-              MoneyMind
-            </ThemedText>
-            <ThemedText
-              className="text-wrap font-bold !text-[#fff]"
-              style={{
-                fontSize: isLargeScreen ? 30 : width < 380 ? 16: 24,
-                maxWidth: isLargeScreen ? 250 : width < 380 ? 160 : 200,
-              }}
-            >
-              Unlock Your Financial Dreams Empowering Your Journey to Wealth and Freedom
-            </ThemedText>
+        </ThemedView>
+
+          <ThemedView className="!items-start w-full px-6 absolute top-[45%] text-center pl-10 bg-transparent">
+            <Animated.View style={{opacity:fadeAnim }}>
+              <ThemedText
+                className="mb-5 !text-[#000]"
+                style={{ fontSize: isLargeScreen ? 56 : width < 380 ? 35 : 42 ,
+                  fontFamily: 'Poppins' ,
+                }}
+              >
+                MONEYMIND
+              </ThemedText>
+              <ThemedText
+                className=" !text-[#000]"
+                style={{
+                  fontSize: isLargeScreen ? 30 : width < 380 ? 22: 24,
+                  maxWidth: isLargeScreen ? 250 : width < 380 ? 200 : 200,
+                  fontFamily: 'PlusJakartaSans-wght',
+                }}
+                >
+                UNLOCK Your Financial Dreams Empowering Your Journey to Wealth and Freedom
+              </ThemedText>
+            </Animated.View>
           </ThemedView>
 
-          <ThemedView className="flex-row gap-5 w-full justify-center mt-28 mb-10 bg-transparent">
-            <ThemedButton
-              style={{
-                width: isLargeScreen ? 200 : width < 380 ? 140 : 160,
-                height: isLargeScreen ? 60 : width < 380 ? 40 : 50,
-              }}
-              mode="normal"
-              onPress={() => router.push("/SignIn")}
-            >
-              Sign In
-            </ThemedButton>
-            <ThemedButton
-              style={{
-                width: isLargeScreen ? 170 : width < 380 ? 140 : 160,
-                height: isLargeScreen ? 60 : width < 380 ? 40 : 50,
-              }}
-              mode="confirm"
-              onPress={() => router.push("/SignUp")}
-            >
-              Sign Up
-            </ThemedButton>
-          </ThemedView>
-        </ThemedView>
+          <Animated.View style={{opacity:fadeAnim }}  className="flex-row gap-5 w-full absolute bottom-[5%] justify-center mt-28 mb-10 bg-transparent">
+           <ThemedButton
+                style={{
+                  width: isLargeScreen ? 200 : width < 380 ? 140 : 160,
+                  height: isLargeScreen ? 60 : width < 380 ? 40 : 50,
+                  fontFamily: 'Inter-Black' 
+                }}
+                mode="normal"
+                onPress={() => router.push("/SignIn")}
+                >
+                Sign In
+              </ThemedButton>
+              <ThemedButton
+                style={{
+                  width: isLargeScreen ? 170 : width < 380 ? 140 : 160,
+                  height: isLargeScreen ? 60 : width < 380 ? 40 : 50,
+                  fontFamily: 'Inter-Black' 
+                }}
+                mode="confirm"
+                onPress={() => router.push("/SignUp")}
+                >
+                Sign Up
+              </ThemedButton>
+          </Animated.View>
       </ThemedScrollView>
   );
 }
